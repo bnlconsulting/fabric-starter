d6ba556848d0e910539608bede51d044ec407791
georgemuller <george.muller@bnl-consulting.com>
2018-01-09 09:44:28 -0500

    Added node chaincode

---
+++ chaincode/go/chaincode/chaincode.js
@@ -0,0 +1,193 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+
+'use strict';
+const shim = require('fabric-shim');
+const util = require('util');
+
+let Chaincode = class {
+
+    // The Init method is called when the Smart Contract is instantiated by the blockchain network
+    // Best practice is to have any Ledger initialization in separate function -- see initLedger()
+    async Init(stub) {
+        console.info('=========== Instantiated fabcar chaincode ===========');
+        return shim.success();
+    }
+
+    async initLedger(stub, args) {
+        console.info('============= START : Initialize Ledger ===========');
+        let providers = [];
+        providers.push({
+            credentialNumber:'h5j38dj49fj493',
+            lastName:'Phelps',
+            firstName:'Jason'
+        });
+
+        providers.push({
+            credentialNumber:'h58dj49fj49',
+            lastName:'Phelps',
+            firstName:'Alex'
+        });
+
+        providers.push({
+            credentialNumber:'h58dj4j43',
+            lastName:'Muller',
+            firstName:'George'
+        });
+
+        for (let i = 0; i < providers.length; i++) {
+            providers[i].docType = 'provider';
+            await stub.putState(providers[i].credentialNumber , Buffer.from(JSON.stringify(providers[i])));
+            console.info('Added <--> ', providers[i].credentialNumber);
+        }
+        console.info('============= END : Initialize Ledger ===========');
+    }
+
+    // The Invoke method is called as a result of an application request to run the Smart Contract.
+    // The calling application program has also specified the particular smart contract
+    // function to be called, with arguments
+    async Invoke(stub) {
+        let ret = stub.getFunctionAndParameters();
+        console.info(ret);
+
+        let method = this[ret.fcn];
+        if (!method) {
+            console.error('no function of name:' + ret.fcn + ' found');
+            throw new Error('Received unknown function ' + ret.fcn + ' invocation');
+        }
+        try {
+            console.log(ret.fcn, JSON.stringify(ret.params))
+            let payload = await method(stub, ret.params, this);
+            return shim.success(payload);
+        } catch (err) {
+            console.log(err);
+            return shim.error(err);
+        }
+    }
+
+    async queryProvider(stub, args) {
+        if (args.length != 1) {
+            throw new Error('Incorrect number of arguments. Expecting CredentialNumber ');
+        }
+        let providerCredentialNumber = args[0];
+
+        let providerAsBytes = await stub.getState(providerCredentialNumber); //get the provider from chaincode state
+        if (!providerAsBytes || providerAsBytes.toString().length <= 0) {
+            throw new Error(providerCredentialNumber + ' does not exist: ');
+        }
+        console.log(providerAsBytes.toString());
+        return providerAsBytes;
+    }
+
+    async createProvider(stub, args) {
+        console.info('============= START : Create Provider ===========');
+        if (args.length != 1) {
+            throw new Error('Incorrect number of arguments. Expecting 1');
+        }
+
+        let newProvider = JSON.parse(args[0]);
+
+        newProvider.docType =  'provider';
+        console.log(JSON.stringify(newProvider));
+
+        let providerAsBytes = await stub.getState(newProvider.credentialNumber); //get the provider from chaincode state
+        if (!providerAsBytes || providerAsBytes.toString().length <= 0) {
+            await stub.putState(newProvider.credentialNumber, Buffer.from(JSON.stringify(newProvider)));
+        }else{
+            throw new Error(newProvider.credentialNumber + ' already exist: ');
+        }
+
+
+        console.info('============= END : Create Provider ===========');
+    }
+
+    async updateProvider(stub, args) {
+        console.info('============= START : Update Provider ===========');
+        if (args.length != 1) {
+            throw new Error('Incorrect number of arguments. Expecting 1');
+        }
+
+        let newProvider = JSON.parse(args[0]);
+
+        newProvider.docType =  'provider';
+
+        let providerAsBytes = await stub.getState(newProvider.CredentialNumber); //get the provider from chaincode state
+        if (!providerAsBytes || providerAsBytes.toString().length <= 0) {
+            throw new Error(newProvider.credentialNumber + ' does not exist: ');
+        }else{
+            await stub.putState(newProvider.credentialNumber, Buffer.from(JSON.stringify(newProvider)));
+        }
+
+        console.info('============= END : Create Provider ===========');
+    }
+
+    async deleteProvider(stub, args) {
+        if (args.length != 1) {
+            throw new Error('Incorrect number of arguments. Expecting CredentialNumber ');
+        }
+        let providerCredentialNumber = args[0];
+        await stub.deleteState(providerCredentialNumber);
+    }
+
+    // =========================================================================================
+    // getQueryResultForQueryString executes the passed in query string.
+    // Result set is built and returned as a byte array containing the JSON results.
+    // =========================================================================================
+    async getQueryResultForQueryString(stub, args, thisClass) {
+
+        let queryString =args[0];
+
+        console.info('- getQueryResultForQueryString queryString:\n' + queryString)
+        let resultsIterator = await stub.getQueryResult(queryString);
+        let method = thisClass['getAllResults'];
+
+        let results = await method(resultsIterator, false);
+
+        return Buffer.from(JSON.stringify(results));
+    }
+
+
+    async getAllResults(iterator, isHistory) {
+        let allResults = [];
+        while (true) {
+            let res = await iterator.next();
+
+            if (res.value && res.value.value.toString()) {
+                let jsonRes = {};
+                console.log(res.value.value.toString('utf8'));
+
+                if (isHistory && isHistory === true) {
+                    jsonRes.TxId = res.value.tx_id;
+                    jsonRes.Timestamp = res.value.timestamp;
+                    jsonRes.IsDelete = res.value.is_delete.toString();
+                    try {
+                        jsonRes.Value = JSON.parse(res.value.value.toString('utf8'));
+                    } catch (err) {
+                        console.log(err);
+                        jsonRes.Value = res.value.value.toString('utf8');
+                    }
+                } else {
+                    jsonRes.Key = res.value.key;
+                    try {
+                        jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+                    } catch (err) {
+                        console.log(err);
+                        jsonRes.Record = res.value.value.toString('utf8');
+                    }
+                }
+                allResults.push(jsonRes);
+            }
+            if (res.done) {
+                console.log('end of data');
+                await iterator.close();
+                console.info(allResults);
+                return allResults;
+            }
+        }
+    }
+};
+
+shim.start(new Chaincode());
