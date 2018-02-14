8d23ca084d880743b917fd71f741a50f34b0f86c
C. Jason Phelps <jason.phelps@bnl-consulting.com>
2018-02-14 08:04:58 -0500

    static forwarding

--- server/index.js
+++ server/index.js
@@ -41,8 +41,9 @@ var apiApp   = require('./app/express-api-app')(); // TODO: this app still uses 
 
 
 
-app.get('/',  (req, res)=>res.redirect('/web') );
+app.get('/',            (req, res)=>res.redirect('/web') );
 app.use('/web',         webApp);
+app.use('/static',      (req, res)=>res.redirect('/web' + res.originalUrl) );
 app.get('/favicon.ico', webApp.handle.bind(webApp) );
 app.use('/admin',       adminApp);
 
