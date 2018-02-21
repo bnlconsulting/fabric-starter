e5e54359018f5bf2da753c710bc40d09a74ab7d1
C. Jason Phelps <jason.phelps@bnl-consulting.com>
2018-02-21 08:39:21 -0500

    minor code edits

--- server/client-app/src/components/Header/index.js
+++ server/client-app/src/components/Header/index.js
@@ -1,6 +1,5 @@
 import './index.css';
 import React, {Component} from 'react';
-
 import logo from '../../logo.svg';
 
 //REDUX
--- server/client-app/src/redux/actions/index.js
+++ server/client-app/src/redux/actions/index.js
@@ -1,4 +1,4 @@
-import * as types from './action-types'
+import * as types from './action-types';
 import history from '../history';
 
 export const getProviderData = ( query) => {
